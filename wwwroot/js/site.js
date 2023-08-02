14a8da9b5889b41934546524073021f2c6d35511
Victorino13 <114184082+Victorino13@users.noreply.github.com>
2023-08-02 00:39:11 -0700

    Initial commit

---
+++ wwwroot/js/site.js
@@ -0,0 +1,12 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your Javascript code.
+
+var string = "Dancing in the Moonlight";
+var str = string.split("");
+var el = document.getElementById('str');
+(function animate() {
+    str.length > 0 ? el.innerHTML += str.shift() : clearTimeout(running);
+    var running = setTimeout(animate, 90);
+})();
